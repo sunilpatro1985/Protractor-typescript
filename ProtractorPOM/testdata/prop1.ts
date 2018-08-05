cbb52d084eedfe177beef2ef60047b0bda26595e
Sunil Patro <sunilpatro1985@gmail.com>
2018-08-05 19:49:41 +0530

    Add files via upload

---
+++ ProtractorPOM/testdata/prop1.ts
@@ -0,0 +1,8 @@
+module.exports = {
+    "siteurl" : "http://www.globalsqa.com/angularJs-protractor/BankingProject/#/manager/addCust",
+    "customers" :{
+        "firstname" : "John",
+        "lastname" : "Carry",
+        "postalcode" : "87654"
+    }
+}
